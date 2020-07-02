98e57aaa9bea0848aacaf6f2d1347c52c6d4b5e1
Kevin Anderson <kmichael419@hotmail.com>
2020-07-02 16:23:54 -0500

    html general layout setup

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,30 @@
 $(document).ready(init);
 
 function init() {
-  console.log('jq');
+  $('#additionButton').on('click', clickAddition);
+  $('#subtractionButton').on('click', clickSubtraction);
+  $('#multiplyButton').on('click', clickMultiply);
+  $('#divisionButton').on('click', clickDivison);
+  $('#equalsButton').on('click', clickEquals);
+  $('#clearButton').on('click', clickClear);
+}
+
+function clickAddition() {
+  console.log('click');
+}
+
+function clickSubtraction() {
+  console.log('click');
+}
+function clickMultiply() {
+  console.log('click');
+}
+function clickDivison() {
+  console.log('click');
+}
+function clickClear() {
+  console.log('click');
+}
+function clickEquals() {
+  console.log('click');
 }
